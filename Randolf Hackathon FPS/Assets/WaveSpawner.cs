c2ec9e813d50c07f1f0ed56878f7e1801a2de220
AndresM77 <andres@triplean.com>
2018-02-24 15:51:13 -0600

    push

--- Randolf Hackathon FPS/Assets/WaveSpawner.cs
+++ Randolf Hackathon FPS/Assets/WaveSpawner.cs
@@ -4,13 +4,64 @@ using UnityEngine;
 
 public class WaveSpawner : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public enum spawnState {SPAWNING, WAITING, COUNTING};
+
+	[System.Serializable]
+	public class Wave 
+	{
+		public string name;
+		public int count;
+		public Transform enemy;
+		public float rate;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	public Wave[] waves;
+	private int nextWave = 0;
+
+	public float timeBetweenWaves = 5;
+	public float waveCountdown = 0;
+
+	private spawnState state = spawnState.COUNTING;
+
+	void Start() 
+	{
+		waveCountdown = timeBetweenWaves;
+
 	}
+
+	void Update() 
+	{
+		if (waveCountdown <= 0) 
+		{
+			if (state != spawnState.SPAWNING) 
+			{
+				//start spawning wave
+				StartCoroutine(SpawnWave(waves[nextWave]));
+			}
+		} else 
+		{
+			waveCountdown -= Time.deltaTime;
+		}
+	}
+
+	IEnumerator SpawnWave(Wave _wave) 
+	{
+		//spawning
+		state = spawnState.SPAWNING;
+
+		for (int i = 0; i < _wave.count; i++) 
+		{
+			SpawnEnemy (_wave.enemy);
+		}
+		//waiting
+		state = spawnState.WAITING;
+		yield break;
+	}
+
+	void SpawnEnemy (Transform _enemy) 
+	{
+		//spawning enemy
+		Debug.Log("Spawning enemy: " + _enemy.name);
+	}
+
 }
