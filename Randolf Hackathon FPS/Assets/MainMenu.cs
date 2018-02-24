21fe289248479526c0f48942526382d15609f67c
AndresM77 <andres@triplean.com>
2018-02-24 13:40:30 -0600

    Edit

--- Randolf Hackathon FPS/Assets/MainMenu.cs
+++ Randolf Hackathon FPS/Assets/MainMenu.cs
@@ -10,5 +10,11 @@ public class MainMenu : MonoBehaviour {
 		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
 	}
 		
+	public void QuitGame ()
+	{
+		Application.Quit();
+		Debug.Log("Quit!");
+	}
+	
 
 }
